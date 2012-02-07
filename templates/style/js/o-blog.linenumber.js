70ab6e76161dd42cde4ba235bcc933ff13fba2b8
Sébastien Gross <sebchezwamorg>
2012-02-07 23:53:04 +0100

    Add google-prettify.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ templates/style/js/o-blog.linenumber.js
@@ -0,0 +1,13 @@
+// Number lines
+
+
+
+
+
+$(document).ready(
+    function() {
+	$(".modal pre.src").addClass('prettyprint').addClass('linenums');
+	prettyPrint();
+	$(".modal").css('width', '');
+    }
+);
