7c7270f7dfe9b957bfb9f27e4643d1bc7a7fa7a3
Sébastien Gross <sebchezwamorg>
2012-02-08 19:26:29 +0100

    Fix some org-mode html  exports.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ templates/style/js/o-blog-fix.js
@@ -0,0 +1,27 @@
+/*
+ * Fix some org-mode html export
+ */
+
+$(document).ready(
+    function() {
+
+	/* create the top menu bar */
+	var menu=$('.navbar-fixed-top .nav-collapse ul:first-child')
+	menu.addClass("nav");
+
+	/* find sub menu items */
+	menu.find('li ul').parent().addClass("dropdown");
+
+	/* and add dropdown features */
+	menu.find('li.dropdown > a').addClass('dropdown-toggle')
+	    .attr("data-toggle", "dropdown")
+	    .append(' <b class="caret"></b>')
+	    .parent().find('ul').addClass("dropdown-menu");
+
+	/* Add divider class if menu item is empty */
+	menu.find('.dropdown-menu li').each(function() {
+	    if ( $(this).text() == '\n') $(this).addClass('divider')
+	});
+
+    }
+)
