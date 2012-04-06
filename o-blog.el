c5cdb7f0639e7356fe46bafd34e1ca13b7bbe3b0
Sébastien Gross <sebchezwamorg>
2012-04-06 13:48:12 +0200

    Be sure buffer is org-mode before exporting to html.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- o-blog.el
+++ o-blog.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs,
 ;; Created: 2012-01-04
-;; Last changed: 2012-03-28 13:43:49
+;; Last changed: 2012-04-06 13:46:12
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -543,6 +543,7 @@ headers and body."
   "Convert STRING to html using `org-mode' syntax."
   (with-temp-buffer
     (insert string)
+    (org-mode)
     (substring-no-properties (org-export-as-html nil nil nil 'string t))))
 
 
