00d30c0d1aab2cb938e3fd2ac22cc0a683123cc0
Sébastien Gross <sebchezwamorg>
2012-11-20 12:30:12 +0100

    Test source exists before copy.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- o-blog.el
+++ o-blog.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs,
 ;; Created: 2012-01-04
-;; Last changed: 2012-11-20 11:55:30
+;; Last changed: 2012-11-20 12:29:11
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -391,7 +391,8 @@ A copy function COPYF and its arguments ARGS could be specified."
 		 (t 'copy-file)))
 	 (args (or args
 		   (when (eq 'copy-file copyf) '(t t t)))))
-    (apply copyf src dst args)))
+    (when (file-exists-p src)
+	(apply copyf src dst args))))
 
 
 (defun org-blog-publish-run-processes-sentinel (proc change)
