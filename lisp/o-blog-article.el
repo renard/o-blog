82546ddab104972265b43ff6a04596941881263b
Sébastien Gross <sebchezwamorg>
2013-01-26 02:05:21 +0100

    Add html slot to article.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- lisp/o-blog-article.el
+++ lisp/o-blog-article.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-21
-;; Last changed: 2013-01-22 01:02:58
+;; Last changed: 2013-01-22 16:46:55
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -42,6 +42,9 @@
    (source :initarg :source
 	   :type string
 	   :documentation "Article raw source")
+  (html :initarg :html
+	   :type string
+	   :documentation "Article html result")
    )
   "Object type handeling o-blog articles.")
 
