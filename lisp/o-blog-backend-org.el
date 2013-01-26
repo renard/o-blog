2aa90b81ff4717f1810612c11e2a5a767ab52b8d
Sébastien Gross <sebchezwamorg>
2013-01-26 02:06:14 +0100

    Start convert article from org to html.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- lisp/o-blog-backend-org.el
+++ lisp/o-blog-backend-org.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-12-04
-;; Last changed: 2013-01-22 14:35:12
+;; Last changed: 2013-01-26 02:04:41
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -143,9 +143,46 @@ headers and body."
 	 do (set-slot-value self type (ob:parse-entries-1 self type)))
    self))
 
-(defmethod ob:convert-article ((self ob:backend:org))
+
+
+(defmethod ob:org-fix-html ((self ob:backend:org) html)
   ""
-  )
+  (with-temp-buffer
+    (insert html)
+    (goto-char (point-min))
+    (save-match-data
+      (while (re-search-forward
+"\\n<div id=\\\"outline-container-%s\\\" class=\\\"outline-%d%s\\\">\\n<h%d id=\"%s\\\">%s%s</h%d>\\n<div class=\\\"outline-text-%d\\\" id=\\\"text-%s\\\">"
+)))))
+
+(defmethod ob:convert-article ((self ob:backend:org) article )
+  "Convert ARTICLE to html using `org-mode' syntax."
+  (with-temp-buffer
+    (insert (oref article source))
+    (org-mode)
+    (goto-char (point-min))
+    ;; exporting block with ditaa is kinda messy since it requires a real
+    ;; file (does not work with a temp-buffer which is not associated to any
+    ;; file).
+    (let ((saved-file
+	   (when
+	       (re-search-forward "^#\\+BEGIN_SRC:?[ \t]+\\(ditaa\\)" nil t)
+	     (format "/%s/%s/%s.src.org"
+		     default-directory
+		     (ob:blog-publish-dir BLOG)
+		     ;; variable inherited from `ob-parse-entry'
+		     htmlfile)))
+	  (org-confirm-babel-evaluate nil)
+	  ret)
+      (when saved-file
+	(ob-write-file saved-file))
+      (set-slot-value
+       article 'html
+       (substring-no-properties
+	(org-export-as-html nil nil nil 'string t)))
+      (when saved-file
+	(delete-file saved-file))
+      article)))
 
 (provide 'o-blog-backend-org)
 
