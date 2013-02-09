dec0031d5d02f0a83717ea8978efd4d6863c9c2d
Sébastien Gross <sebchezwamorg>
2013-02-09 19:28:26 +0100

    Add string sanitizer.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- lisp/o-blog-utils.el
+++ lisp/o-blog-utils.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-22
-;; Last changed: 2013-01-22 01:11:24
+;; Last changed: 2013-02-09 12:25:47
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -23,6 +23,31 @@ result string."
   (loop for (regexp rep) in replacement-list
 	do (setf string (replace-regexp-in-string regexp rep string)))
   string)
+
+(defun ob:sanitize-string (s)
+  "Sanitize string S by:
+
+- converting all charcters ton pure ASCII
+- replacing non alphanumerical chars to \"-\"
+- down-casing all letters
+- trimming leading and tailing \"-\""
+  (loop for c across s
+	with cd
+	with gc
+	with ret
+	do (progn
+	     (setf gc (get-char-code-property c 'general-category))
+	     (setf cd (get-char-code-property c 'decomposition)))
+	if (or (member gc '(Lu Ll Nd)) (= ?- c))
+	collect (downcase
+		 (char-to-string (if cd (car cd)  c)))
+	into ret
+	else if (member gc '(Zs))
+	collect "-" into ret
+	finally return (ob:replace-in-string
+			(mapconcat 'identity ret "")
+			'(("--+" "-")
+			  ("^-+\\|-+$" "")))))
 
 (provide 'o-blog-utils)
 
