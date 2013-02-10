bbf85dc860af255233dfaac270a25c816d0d06ab
Sébastien Gross <sebchezwamorg>
2013-02-10 19:41:43 +0100

    Compute article path.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- lisp/o-blog-entry.el
+++ lisp/o-blog-entry.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-21
-;; Last changed: 2013-02-09 11:28:56
+;; Last changed: 2013-02-10 15:56:06
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -31,6 +31,15 @@
    (html :initarg :html
 	 :type string
 	 :documentation "Article html result")
+   (path :initarg :path
+	   :type string
+	   :documentation "")
+   (file :initarg :file
+	   :type string
+	   :documentation "")
+   (htmlfile :initarg :htmlfile
+	   :type string
+	   :documentation "")
    )
   "Object type handeling o-blog entries.")
 
@@ -54,8 +63,24 @@
 	 :documentation "")
    (tags :initarg :tag
 	 :type list
-	 :documentation "List of ob:tag"))
+	 :documentation "List of ob:tag")
+   (category :initarg :category
+	     ;;	   :type ob:category
+	   :documentation "")
+)
   "O-blog page article")
+
+(defmethod ob:entry:set-path ((self ob:entry))
+  ""
+  (set-slot-value
+   self 'path (format "%s"
+		      (ob:sanitize-string (oref self title))))
+  (set-slot-value
+   self 'htmlfile (format "%s/%s"
+		      (oref self path)
+		      (oref self path))))
+
+
 
 (defmethod ob:entry:compute-dates ((self ob:article))
   ""
@@ -67,6 +92,21 @@
 		(string-to-number
 		 (format-time-string f timestamp)))))))
 
+(defmethod ob:entry:set-path ((self ob:article))
+  ""
+  (set-slot-value
+   self 'path (format "%s/%.4d/%.2d"
+		      (ob:sanitize-string (oref (oref self category) safe))
+		      (oref self year)
+		      (oref self month)))
+  (set-slot-value
+   self 'file (format "%.2d_%s.html"
+		      (oref self day)
+		      (ob:sanitize-string (oref self title))))
+  (set-slot-value
+   self 'htmlfile (format "%s/%s"
+		      (oref self path)
+		      (oref self file))))
 
 
 (defclass ob:page (ob:entry)
