1796397d1590916b2b9b17f853f97fa2aea71b63
Sébastien Gross <sebchezwamorg>
2013-03-25 17:06:44 +0100

    Add ob:eval-lisp function.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- lisp/o-blog-utils.el
+++ lisp/o-blog-utils.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-22
-;; Last changed: 2013-02-09 12:25:47
+;; Last changed: 2013-03-25 16:22:23
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -55,6 +55,53 @@ result string."
   (let ((buffer (current-buffer)))
     (with-temp-file file
       (insert (with-current-buffer buffer (buffer-string))))))
+
+(defun ob:eval-lisp()
+  "Eval embeded lisp code defined by <lisp> tags in html fragment
+when publishing a page."
+  (save-excursion
+    (save-restriction
+      (save-match-data
+	;; needed for thing-at-point
+	(html-mode)
+	(beginning-of-buffer)
+	(let ((open-tag "<lisp>\\|{lisp}\\|\\[lisp\\]")
+	      (close-tag "</lisp>\\|{/lisp}\\|\\[/lisp\\]")
+	      beg end sexp)
+	  (while (search-forward-regexp open-tag nil t)
+	    (setq beg (- (point) (length  (match-string 0))))
+	    (when (search-forward-regexp close-tag nil t)
+	      (setq end (point))
+	      (backward-char (length (match-string 0)))
+	      (backward-sexp)
+	      (setq sexp (substring-no-properties (thing-at-point 'sexp)))
+	      (narrow-to-region beg end)
+	      (delete-region (point-min) (point-max))
+	      (insert
+	       (save-match-data
+		 (condition-case err
+		     (let ((object (eval (read sexp))))
+		       (cond
+			;; result is a string
+			((stringp object) object)
+			;; a list
+			((and (listp object)
+			      (not (eq object nil)))
+			 (let ((string (pp-to-string object)))
+			   (substring string 0 (1- (length string)))))
+			;; a number
+			((numberp object)
+			 (number-to-string object))
+			;; nil
+			((eq object nil) "")
+			;; otherwise
+			(t (pp-to-string object))))
+		   ;; error handler
+		   (error
+		    (format "Lisp error in %s: %s" (buffer-file-name) err)))))
+	      (goto-char (point-min))
+	      (widen))))))))
+
 (provide 'o-blog-utils)
 
 ;; o-blog-utils.el ends here
