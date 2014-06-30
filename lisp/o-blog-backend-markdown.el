d48d29175516ca7504f2d74da68ebb1a538b74ce
Sébastien Gross <sebchezwamorg>
2014-06-30 23:11:27 +0200

    Add markdown format support

---
+++ lisp/o-blog-backend-markdown.el
@@ -0,0 +1,243 @@
+;;; o-blog-backend-markdown.el --- Markdown backend for o-blog
+
+;; Copyright © 2013 Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+
+;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+;; Keywords: emacs, 
+;; Created: 2013-08-22
+;; Last changed: 2014-06-30 23:09:45
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;; 
+
+
+;;; Code:
+
+(eval-when-compile
+  (require 'eieio nil t)
+  (require 'o-blog-backend nil t)
+  (require 'markdown-mode nil t))
+
+
+(setq ob:markdown:bootstrap:widgets
+  '(("jumbotron" "<div class=\"jumbotron\">" "</div>")
+    ("page-header" "<div class=\"page-header\">" "</div>")
+    ("alert" "<div class=\"alert alert-%type%\" role=\"alert\">" "</div>")
+    ("panel"  "<div class=\"panel panel-%type%\">" "</div>")
+
+
+    ))
+
+
+
+
+
+(defclass ob:backend:markdown (ob:backend)
+  ()
+  "Object type handeling o-blog files in markdown.")
+
+(defmethod ob:find-files ((self ob:backend:markdown))
+  (set-slot-value self 'source-files
+		  (ob:find-files-1 self "txt")))
+
+
+(defmethod ob:parse-config ((self ob:backend:markdown) &optional file)
+  "Parse global configuration file."
+  (let* ((file (or file
+		   (when (file-exists-p "o-blog.conf")
+		     "o-blog.conf")))
+	 (lines (when file
+		  (split-string
+		   (with-temp-buffer
+		     (insert-file-contents file)
+		     (buffer-string))
+		   "\n"))))
+    (when lines
+      (save-match-data
+	(loop for line in lines
+	      when (string-match "^\\s-*\\([^#]+?\\)\\s-*=\\s-*\\(.+?\\)\\s-*$" line)
+	      do (let ((k (intern (match-string 1 line)))
+		       (v (match-string 2 line)))
+		   (when (slot-exists-p self k)
+		     (set-slot-value self k v))))))))
+
+
+
+(defun ob:parse-tags (tags)
+  "Parse TAGS and generate a list of `ob:tags'."
+  (let ((tags (if (listp tags)
+		  tags (split-string tags "\\s-?,\\s-"))))
+    (loop for tag in tags
+	  for td = (ob:replace-in-string tag '(("_" " ") ("@" "-")))
+	  for ts = (ob:sanitize-string td)
+	  collect (ob:tag tag :display td :safe ts))))
+
+
+(defmethod ob:parse-entries ((self ob:backend:markdown))
+  (loop for f in (ob:get 'source-files self)
+	do (with-temp-buffer
+	     (insert-file-contents f)
+	     (let* ((headers (ob:markdown:get-headers))
+		    (type (intern (plist-get headers 'type)))
+		    (types (intern (format "%ss" type)))
+		    (obj (funcall (intern (format "ob:%s" type))
+			  f
+			  :file f
+			  :source (buffer-substring-no-properties
+				   (point-min) (point-max))))
+		    (obj-list (ob:get types self)))
+	       
+	       (when (slot-exists-p obj 'timestamp)
+		 (set-slot-value obj 'timestamp
+				 (nth 5 (file-attributes f)))
+		 (ob:entry:compute-dates obj))
+
+	       (when (slot-exists-p obj 'tags)
+		 (set-slot-value obj 'tags
+				 (ob:parse-tags (plist-get headers 'tags))))
+
+	       (when (slot-exists-p obj 'category)
+		 (set-slot-value obj 'category
+				 (ob:category:init
+				  (ob:category
+				   (plist-get headers 'category)))))
+
+	       (loop for header in headers by #'cddr
+		     when (and
+			   (slot-exists-p obj header)
+			   (not (member header '(tags category))))
+		     do (set-slot-value
+			 obj header (plist-get headers header)))
+	       ;;(message "%S" (ob:get 'title obj))
+	       (if (eq 'page type)
+		   (ob:entry:set-path obj
+				      (concat
+				       (ob:sanitize-string
+					(or (plist-get headers 'page)
+					    (ob:get 'title obj)))
+				       ".html"))
+		 (ob:entry:set-path obj))
+	       
+	       (set-slot-value self types (append obj-list (list obj)))
+
+	       (set-slot-value obj 'files-to-copy
+			       (ob:markdown:get-images obj))
+
+
+	       (message "*** %S" obj)
+	       (message "FILES TO COPY: %S" (ob:get 'files-to-copy obj))
+
+	       ))))
+
+
+
+
+(defmethod ob:convert-entry ((self ob:backend:markdown) entry)
+  ""
+  (with-temp-buffer
+    (insert (ob:get 'source entry))
+    (ob:framework-expand "<\\([a-z][a-z0-9-]*\\)\\([^>]+\\)?>"  "</%s>" "" "")
+    (let ((buff-src (current-buffer)))
+      (with-temp-buffer
+	(let ((buff-out (current-buffer)))
+	  (with-current-buffer buff-src
+	    (call-process-region (point-min) (point-max) "pandoc" nil buff-out))
+	  (set-slot-value
+	   entry 'html
+	   (buffer-substring-no-properties (point-min)(point-max))))))))
+
+
+(defun ob:markdown:expand-bootstrap ()
+  "Expand bootstrap features in markdown source format."
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (while (search-forward-regexp "<\\([a-z][a-z0-9-]*\\)\\s-*\\([^>/]+\\)?\\(/\\)?>" nil t)
+	(let* ((all (match-string-no-properties 0))
+	       (tag (match-string-no-properties 1))
+	       (attributes (match-string-no-properties 2))
+	       (closed (match-string-no-properties 3))
+	       (data (assoc tag ob:markdown:bootstrap:widgets)))
+	  (when data
+	    (let ((xml (with-temp-buffer
+			 (insert (format "<%s %s/>" tag (or attributes "")))
+			 (goto-char (point-min))
+			 (xml-parse-region))))
+	      (message "%S" xml)
+	      (narrow-to-region (- (point) (length all)) (point))
+	      (delete-region (point-min) (point-max))
+	      (let ((attrs (cadar xml))
+		    (string (nth 1 data)))
+		(loop for (k . v) in attrs
+		      do (setf
+			  string
+			  (if (string-match (format "%%%s%%" k) string)
+			      (replace-regexp-in-string
+			       (format "%%%s%%" k) v string)
+			    (replace-regexp-in-string
+			     "\\(/?>\\)"
+			     (format " %s=%S\\1" k v) string))))
+		(insert string))
+	      (widen)
+	      (unless closed
+		(search-forward (format "</%s>" tag))
+		(narrow-to-region (- (point) (length tag) 3) (point))
+		(delete-region (point-min) (point-max))
+		(insert (nth 2 data))
+		(widen)))))))))
+
+(defun ob:markdown:get-images (object)
+  ""
+  (with-temp-buffer
+    (insert (ob:get 'source object))
+    (goto-char (point-min))
+    (let (files)
+      ;; images
+      (save-match-data
+	(while (search-forward-regexp markdown-regex-link-inline nil t)
+	  (let ((link (match-string-no-properties 3)))
+	    (with-temp-buffer
+	      (insert link)
+	      (goto-char (point-min))
+	      (save-match-data
+		(while (search-forward-regexp "(\\(.*\\)\\(?: \"[^\"]*\"\\))" nil t)
+		  (push (match-string-no-properties 1) files)))))))
+      files)))
+
+
+(defun ob:markdown:get-headers ()
+  "Return all headers from a markdown file.
+
+Headers must be defined within the first Comment block enclosed
+into \"<!--/\" \"/-->\"."
+  (save-excursion
+    (save-restriction
+      (save-match-data
+	(widen)
+	(goto-char (point-min))
+	(let* ((comment-start "<!--/")
+	       (comment-end "/-->")
+	       (start (when (search-forward comment-start nil t)
+			(backward-char (length comment-start))
+			(point)))
+	       (end (when (search-forward comment-end nil t)
+		      (point))))
+	  (when (and start end)
+	    
+	    (loop for line in (split-string
+			       (buffer-substring-no-properties start end)
+			       "\n")
+		  when (string-match
+			"^\\s-*\\([^#]+?\\)\\s-*\\(?:[=:]\\)\\s-*\\(.+?\\)\\s-*$" line)
+		  nconc (let ((k (intern (match-string 1 line)))
+			      (v (match-string 2 line)))
+			 (list k v)))))))))
+
+  
+
+(provide 'o-blog-backend-markdown)
+
+;; o-blog-backend-markdown.el ends here
