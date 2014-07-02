069220e1046988ecd4658b09f9e2470c476c34e1
Sébastien Gross <sebchezwamorg>
2014-07-02 20:59:11 +0200

    Remove dead code

--- lisp/o-blog-backend-markdown.el
+++ lisp/o-blog-backend-markdown.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-08-22
-;; Last changed: 2014-07-01 23:35:41
+;; Last changed: 2014-07-01 23:36:54
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -112,10 +112,7 @@
 	       (set-slot-value self types (append obj-list (list obj)))
 
 	       (set-slot-value obj 'files-to-copy
-			       (ob:markdown:get-images obj))
-	       ;; (message "*** %S" obj)
-	       ;; (message "FILES TO COPY: %S" (ob:get 'files-to-copy obj))
-	       ))))
+			       (ob:markdown:get-images obj))))))
 
 
 
