78b867f4e1a8cb9dbe65c589e5764cc3007328ed
Sébastien Gross <sebchezwamorg>
2014-07-04 18:15:06 +0200

    Fix article JSON generation

--- lisp/o-blog-backend.el
+++ lisp/o-blog-backend.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-12-04
-;; Last changed: 2014-07-04 14:19:05
+;; Last changed: 2014-07-04 17:14:32
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -181,10 +181,10 @@ Some global variables are set:
 			    )))
 		 ;; remove last comma
 		 (delete-char -1)
-		 (insert "]},")))
+		 (insert "],")))
       ;; remove last comma
       (delete-char -1)
-      (insert "}"))
+      (insert "}}"))
 
     
     (let ((BREADCRUMB "Archives"))
