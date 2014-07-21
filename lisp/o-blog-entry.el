10af73e39ac7d1aadf0dffeaf76bbd1204bfa95c
Sébastien Gross <sebchezwamorg>
2014-07-21 22:46:53 +0200

    Fix html2text in o-blog-entry

--- lisp/o-blog-entry.el
+++ lisp/o-blog-entry.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-21
-;; Last changed: 2014-07-21 11:54:48
+;; Last changed: 2014-07-21 22:29:40
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -18,7 +18,7 @@
 
 
 (eval-when-compile
-  (require 'htmlize)
+  (require 'html2text)
   (require 'sgml-mode)
   (require 'cl))
 
